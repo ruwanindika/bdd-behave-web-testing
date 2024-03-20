5da5f343de9748c293cf94a8932837f3d732faf1
ruwanindika <ruwanindika@gmail.com>
2024-03-21 10:09:52 +1100

    Initial commit

---
+++ features/devops1-webpage.feature
@@ -0,0 +1,38 @@
+Feature: DevOps1 webpage
+
+Scenario: DevOps1 logo presence on the webpage - chrome browser
+    Given launch chrome browser
+    When open DevOps1 page
+    Then verify the logo is present on the page
+    And close the browser
+
+Scenario: Contact us button presence on the webpage - chrome browser
+    Given launch chrome browser
+    When open DevOps1 page
+    Then Click "Contact us" button
+    Then verify the contact us page title is present
+    And close the browser
+
+Scenario: Services dropdown check All Services are present - chrome browser
+    Given launch chrome browser
+    When open DevOps1 page
+    Then click on the services dropdown
+    Then check for 5 items in the list
+    And close the browser
+
+Scenario: Services dropdown select All Services - chrome browser
+    Given launch chrome browser
+    When open DevOps1 page
+    Then click on the services dropdown
+    Then check for 5 items in the list
+    Then Click on All Services
+    Then "Uplifting engineering practices" is present in the page
+    And close the browser
+
+Scenario: "Our services" link test "Platform Engineering" - chrome browser
+    Given launch chrome browser
+    When open DevOps1 page
+    Then check for "Platform Engineering" card
+    Then click on the platform engineering link
+    Then check for text "Platform Engineering"
+    And close the browser
---
+++ features/steps/devops1-webpage-steps.py
@@ -0,0 +1,91 @@
+from behave import given,when,then
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options 
+
+
+
+
+@given(u'launch chrome browser')
+def step_impl(context):
+    options = Options()
+    options.headless = True
+    context.driver=webdriver.Chrome(options=options)
+
+@when(u'open DevOps1 page')
+def step_impl(context):
+    context.driver.get('https://devops1.com.au/')
+    context.driver.maximize_window()
+
+@then(u'verify the logo is present on the page')
+def step_impl(context):
+    status = context.driver.find_element(By.XPATH,'//img[@src="/assets/img/logos/devops1-logo.svg"]').is_displayed()
+    # status = context.driver.find_element(By.XPATH,'/html/body/header-component/header/nav/div/a/img').is_displayed()
+    assert status is True
+
+@then(u'close the browser')
+def step_impl(context):
+    context.driver.close()
+
+@then(u'Click "Contact us" button')
+def step_impl(context):
+    context.driver.find_element(By.XPATH,'//div[@class="column navbar-actions"]//a[@class="btn btn-primary"]').click()
+    # context.driver.find_element(By.XPATH,'/html/body/header-component/header/nav/div/div/div[2]/a[2]').click()
+
+@then(u'verify the contact us page title is present')
+def step_impl(context):
+    linkText = context.driver.find_element(By.XPATH,'//div[@class="contact-form"]//h2[@class="form-title"]').text
+    # linkText = context.driver.find_element(By.XPATH,'/html/body/section[1]/div/div/div[2]/div/h2').text
+    assert linkText == "Get in touch"
+
+@then(u'click on the services dropdown')
+def step_impl(context):
+    toggle_link = context.driver.find_element(By.CSS_SELECTOR,'.nav-link.dropdown-toggle')
+    toggle_link.click()
+    
+@then(u'check for 5 items in the list')
+def step_impl(context):
+    item_list = []
+    service_list = context.driver.find_element(By.CSS_SELECTOR,'.dropdown-menu.show')
+    
+    for i in (service_list.text).splitlines():
+        item_list.append(i)
+    assert item_list == ['All Services', 'Platform Engineering', 'Security Engineering', 'Quality and Observability', 'Engagement models']
+
+@then(u'Click on All Services')
+def step_impl(context):
+    dropdown_link = context.driver.find_element(By.XPATH,'//a[@class="dropdown-item" and text()="All Services"]')
+    # dropdown_link = context.driver.find_element(By.XPATH,'/html/body/header-component/header/nav/div/div/div[1]/ul/li[1]/ul/li[1]/a')
+    dropdown_link.click()
+
+@then(u'"Uplifting engineering practices" is present in the page')
+def step_impl(context):
+    title_one = context.driver.find_element(By.CSS_SELECTOR,'.display-1').text
+    assert title_one == "Uplifting engineering practices"
+
+@then(u'check for "Platform Engineering" card')
+def step_impl(context):
+    card_title_text = context.driver.find_element(By.XPATH,'//div[@class="card-body"]//h3[@class="card-title text-white"]').text
+    # card_title_text = context.driver.find_element(By.XPATH,'//*[@id="services"]/div/div[2]/div[1]/div[1]/h3').text
+
+    assert card_title_text == "Platform Engineering"
+
+@then(u'click on the platform engineering link')
+def step_impl(context):
+    context.driver.execute_script('window.scrollBy(0, 1000)')
+    # context.driver.implicitly_wait(5)
+    # our_services_link = context.driver.find_element(By.XPATH,'//div[@class="feature-grid"]/div[1]/div[2]/a')
+    our_services_link = context.driver.find_element(By.XPATH,'//div[@class="feature-grid"]//child::div[1]//div[@class="card-footer text-white"]//a[@class="btn btn-link btn-arrow"]')
+    ActionChains(context.driver).click(our_services_link).perform()
+    
+@then(u'check for text "Platform Engineering"')
+def step_impl(context):
+    services_page_title = context.driver.find_element(By.XPATH,'//div[@class="hero-content"]//h1[@class="display-1 text-white"]').text
+    assert services_page_title == "Empowering innovation with solid engineering"
+
+    
